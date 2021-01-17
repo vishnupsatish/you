133bd69ee6f0212c25b1333b6420eef0feaa1205
vishnupsatish <vishnupavan.satish@gmail.com>
2021-01-17 10:27:01 -0500

    add tesseract fix

--- application/routes.py
+++ application/routes.py
@@ -25,6 +25,7 @@ admin.add_view(ModelView(PhotosEntry, db.session))
 admin.add_view(ModelView(MainEventEntry, db.session))
 admin.add_view(ModelView(GoalEntry, db.session))
 
+pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
 @app.context_processor
 def inject_date():
